35da8c0a2c1c7806fb9217e73c5b3e54b21bd077
Grace Cuenca <gcuenca@princeton.edu>
2021-04-28 09:12:45 -0400

    maybe this will work now

--- debias-BERT/experiments/eval_bias.py
+++ debias-BERT/experiments/eval_bias.py
@@ -164,7 +164,7 @@ def evaluate(args, def_pairs, word_level=False):
 	specific_set = set(gender_specific_words)
 
 	abs_esizes = []
-	for test_id in ['6', '6edited', '6b', '7', '7b', '8', '8b']:
+	for test_id in ['6', '6e', '6b', '7', '7b', '8', '8b']:
 		filename = "sent-weat{}.jsonl".format(test_id)
 		print(filename)
 		sent_file = os.path.join(DATA_DIR, filename)
@@ -289,8 +289,8 @@ class WordEvaluator(object):
 		
 		####################################################################
 		# defining filenames for evaluation on the edited jsonl file we made
-		self.filenames.append("sent-weat6edited.jsonl")
-		self.word_filenames.append("sent-weat6edited.jsonl")
+		self.filenames.append("sent-weat6e.jsonl")
+		self.word_filenames.append("sent-weat6e.jsonl")
 		####################################################################
 
 		self.vocab = self.init_vocab() # 190 words
